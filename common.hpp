9fcda78c785eee4711357fbf1fe706043c316dfc
Yuri Panchul <yuri@panchul.com>
2025-05-04 10:09:05 -0700

    Started restructuring the examples using common.hpp header

---
+++ common.hpp
@@ -0,0 +1,234 @@
+#include <cstdio>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+//----------------------------------------------------------------------------
+
+extern const char * vertexSource;
+extern const char * fragmentSource;
+
+bool initUserOGL    ();
+void displayUserOGL ();
+void cleanupUserOGL ();
+
+bool initUserOGL ()
+{
+    GLuint vertexArrayObject;
+    glGenVertexArrays (1, & vertexArrayObject);
+    glBindVertexArray (vertexArrayObject);
+    
+    //------------------------------------------------------------------------
+
+    GLuint vertexBufferObject;
+    glGenBuffers (1, & vertexBufferObject);
+    glBindBuffer (GL_ARRAY_BUFFER, vertexBufferObject);
+    
+    float vertices [] = {
+        - 0.5f,   0.5f, 1.0f, 0.0f, 0.0f,  // Vertex 0 (X, Y) Red
+          0.5f,   0.5f, 0.0f, 1.0f, 0.0f,  // Vertex 1 (X, Y) Green
+          0.5f, - 0.5f, 0.0f, 0.0f, 1.0f,  // Vertex 2 (X, Y) Blue
+        - 0.5f, - 0.5f, 1.0f, 1.0f, 1.0f,  // Vertex 3 (X, Y) White
+    };
+          
+    glBufferData (GL_ARRAY_BUFFER,
+        sizeof (vertices), vertices,
+        GL_STATIC_DRAW);    
+
+    //------------------------------------------------------------------------
+
+    GLuint elementBufferObject;
+    glGenBuffers (1, & elementBufferObject);
+    glBindBuffer (GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
+
+    GLuint elements [] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    glBufferData (GL_ELEMENT_ARRAY_BUFFER,
+        sizeof (elements), elements,
+        GL_STATIC_DRAW);
+
+    //------------------------------------------------------------------------
+
+    GLint posAttrib = glGetAttribLocation (shaderProgram, "position");
+
+    glEnableVertexAttribArray (posAttrib);
+
+    glVertexAttribPointer
+    (
+        posAttrib,
+        2,          // Number of the values
+        GL_FLOAT,
+        GL_FALSE,   // Whether the parameters should be normalized
+                    // between 0.0 and 1.0
+                    
+        5 * sizeof (float),   // Stride - number of bytes in between
+        0                     // Offset from the beginning of the array
+    );
+
+    //------------------------------------------------------------------------
+
+    GLint colAttrib = glGetAttribLocation (shaderProgram, "color");
+
+    glEnableVertexAttribArray (colAttrib);
+
+    glVertexAttribPointer
+    (
+        colAttrib,
+        3,          // Number of the values
+        GL_FLOAT,
+        GL_FALSE,   // Whether the parameters should be normalized
+                    // between 0.0 and 1.0
+
+        5 * sizeof (float),            // Stride - number of bytes in between
+        (void *) (2 * sizeof (float))  // Offset from the beginning of the array
+    );
+
+    //------------------------------------------------------------------------
+
+    // Optional for a single output
+    glBindFragDataLocation (shaderProgram, 0, "outColor");
+}
+
+//----------------------------------------------------------------------------
+
+void displayUserOGL ()
+{
+    glDrawElements
+    (
+        GL_TRIANGLES,
+        6,
+        GL_UNSIGNED_INT,
+        0  // Offset of the first index in the array
+    );
+}
+
+//----------------------------------------------------------------------------
+
+void cleanupUserOGL ()
+{
+    glDeleteBuffers      (1, & vertexBufferObject);
+    glDeleteVertexArrays (1, & vertexArrayObject);
+}
+
+//----------------------------------------------------------------------------
+
+bool initShaders ()
+{
+    GLuint vertexShader = glCreateShader (GL_VERTEX_SHADER);
+    glShaderSource (vertexShader, 1, & vertexSource, NULL);
+    
+    // The last argument of glShaderSource is an array of index length,
+    // not needed here.
+    
+    glCompileShader (vertexShader);
+    
+    GLint status;
+    glGetShaderiv (vertexShader, GL_COMPILE_STATUS, & status);
+        
+    char buffer [512];
+    
+    // The third argument of glGetShaderInfoLog
+    // is a pointer to string length, not needed here.
+
+    glGetShaderInfoLog (vertexShader, sizeof (buffer), NULL, buffer);
+
+    if (buffer [0] != '\0')
+        printf ("glGetShaderInfoLog (vertexShader, ...): %s\n", buffer);
+    
+    //------------------------------------------------------------------------
+
+    GLuint fragmentShader = glCreateShader (GL_FRAGMENT_SHADER);
+    
+    glShaderSource     (fragmentShader, 1, & fragmentSource, NULL);
+    glCompileShader    (fragmentShader);
+    glGetShaderiv      (fragmentShader, GL_COMPILE_STATUS, & status);
+    glGetShaderInfoLog (fragmentShader, sizeof (buffer), NULL, buffer);
+
+    if (buffer [0] != '\0')
+        printf ("glGetShaderInfoLog (fragmentShader, ...): %s\n", buffer);
+    
+    //------------------------------------------------------------------------
+
+    GLuint shaderProgram = glCreateProgram ();
+    glAttachShader (shaderProgram, vertexShader);
+    glAttachShader (shaderProgram, fragmentShader);
+    
+    glLinkProgram  (shaderProgram);
+
+    glDeleteShader (fragmentShader);
+    glDeleteShader (vertexShader);
+
+    glUseProgram   (shaderProgram);
+}
+
+//----------------------------------------------------------------------------
+
+bool cleanupShaders ()
+{
+    glDeleteProgram (shaderProgram);
+}
+
+//----------------------------------------------------------------------------
+
+int main ()
+{
+    glfwInit ();
+    
+    //------------------------------------------------------------------------
+
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint (GLFW_RESIZABLE, GL_FALSE);
+
+    //------------------------------------------------------------------------
+
+    GLFWwindow * window = glfwCreateWindow
+    (
+        800,
+        600,
+        "OpenGL",  // title
+        nullptr,   // or glfwGetPrimaryMonitor () for full screen
+        nullptr    // share (resources with another window)
+    );
+
+    glfwMakeContextCurrent (window);
+
+    //------------------------------------------------------------------------
+
+    glewExperimental = GL_TRUE;
+    glewInit ();
+
+    //------------------------------------------------------------------------
+
+    if (! (initShaders () && initUserOGL ())
+        return 1;
+
+    //------------------------------------------------------------------------
+
+    while (! glfwWindowShouldClose (window))
+    {
+        displayUserOGL ();
+
+        glfwSwapBuffers (window);
+        glfwPollEvents  ();
+        
+        if (glfwGetKey (window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose (window, GL_TRUE);
+    }
+
+    //------------------------------------------------------------------------
+
+    cleanupUserOGL ();
+
+    //------------------------------------------------------------------------
+
+    glfwTerminate  ();
+
+    return 0;
+}
+     
