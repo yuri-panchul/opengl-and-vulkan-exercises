2e24583a41911f28bfa81a1a053f75b7acf4cda4
Yuri Panchul <yuri@panchul.com>
2025-05-09 23:33:07 -0700

    added t19_restructured_rb_triangles.cpp

--- t19_restructured_rb_triangles.cpp
+++ t19_restructured_rb_triangles.cpp
@@ -1,7 +1,4 @@
-#include <cstdio>
-
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include "common.hpp"
 
 //----------------------------------------------------------------------------
 
@@ -35,41 +32,17 @@ const char * fragmentSource = R"glsl(
 
 //----------------------------------------------------------------------------
 
-int main ()
+enum VAO_IDs    { Triangles   , NumVAOs    };
+enum Buffer_IDs { ArrayBuffer , NumBuffers };
+
+GLuint VAOs    [ NumVAOs    ];
+GLuint Buffers [ NumBuffers ];
+
+//----------------------------------------------------------------------------
+
+bool initUserOGL ()
 {
-    glfwInit ();
-    
-    glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint (GLFW_RESIZABLE, GL_FALSE);
-
-    GLFWwindow * window = glfwCreateWindow
-    (
-        800,
-        600,
-        "OpenGL",  // title
-        nullptr,   // or glfwGetPrimaryMonitor () for full screen
-        nullptr    // share (resources with another window)
-    );
-
-    glfwMakeContextCurrent (window);
-
-    //------------------------------------------------------------------------
-
-    glewExperimental = GL_TRUE;
-    glewInit ();
-
-    //------------------------------------------------------------------------
-
-    enum VAO_IDs    { Triangles   , NumVAOs    };
-    enum Buffer_IDs { ArrayBuffer , NumBuffers };
     enum Attrib_IDs { vPosition = 0 };
-
-    GLuint VAOs    [ NumVAOs    ];
-    GLuint Buffers [ NumBuffers ];
-
     const GLuint NumVertices = 6;
 
     //------------------------------------------------------------------------
@@ -89,57 +62,6 @@ int main ()
 
     //------------------------------------------------------------------------
 
-    GLuint vertexShader = glCreateShader (GL_VERTEX_SHADER);
-    glShaderSource (vertexShader, 1, & vertexSource, NULL);
-    
-    // The last argument of glShaderSource is an array of index length,
-    // not needed here.
-    
-    glCompileShader (vertexShader);
-    
-    GLint status;
-    glGetShaderiv (vertexShader, GL_COMPILE_STATUS, & status);
-        
-    char buffer [512];
-    
-    // The third argument of glGetShaderInfoLog
-    // is a pointer to string length, not needed here.
-
-    glGetShaderInfoLog (vertexShader, sizeof (buffer), NULL, buffer);
-
-    if (buffer [0] != '\0')
-        printf ("glGetShaderInfoLog (vertexShader, ...): %s\n", buffer);
-    
-    //------------------------------------------------------------------------
-
-    GLuint fragmentShader = glCreateShader (GL_FRAGMENT_SHADER);
-    
-    glShaderSource     (fragmentShader, 1, & fragmentSource, NULL);
-    glCompileShader    (fragmentShader);
-    glGetShaderiv      (fragmentShader, GL_COMPILE_STATUS, & status);
-    glGetShaderInfoLog (fragmentShader, sizeof (buffer), NULL, buffer);
-
-    if (buffer [0] != '\0')
-        printf ("glGetShaderInfoLog (fragmentShader, ...): %s\n", buffer);
-    
-    //------------------------------------------------------------------------
-
-    GLuint shaderProgram = glCreateProgram ();
-    glAttachShader (shaderProgram, vertexShader);
-    glAttachShader (shaderProgram, fragmentShader);
-
-    // Optional for a single output
-    glBindFragDataLocation (shaderProgram, 0, "fColor");
-
-    glLinkProgram  (shaderProgram);
-
-    glDeleteShader (fragmentShader);
-    glDeleteShader (vertexShader);
-
-    glUseProgram   (shaderProgram);
-
-    //------------------------------------------------------------------------
-
     glVertexAttribPointer
     (
         vPosition,         // index
@@ -154,34 +76,28 @@ int main ()
 
     //------------------------------------------------------------------------
 
-    while (! glfwWindowShouldClose (window))
-    {
-        static const float black [] = { 0.0f, 0.0f, 0.0f, 0.0f };
+    // Optional for a single output
+    glBindFragDataLocation (shaderProgram, 0, "fColor");
 
-        glClearBufferfv (GL_COLOR, 0, black);
-
-        // Not sure if we need to bind it again
-        glBindVertexArray (VAOs [Triangles]);
-
-        glDrawArrays (GL_TRIANGLES, 0, NumVertices);
-
-        glfwSwapBuffers (window);
-        glfwPollEvents  ();
-        
-        if (glfwGetKey (window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose (window, GL_TRUE);
-    }
-
-    //------------------------------------------------------------------------
-
-    glDeleteProgram (shaderProgram);
-
-    //------------------------------------------------------------------------
-
-    glfwTerminate ();
-
-    //------------------------------------------------------------------------
-
-    return 0;
+    return true;
 }
-     
+
+//----------------------------------------------------------------------------
+
+void displayUserOGL ()
+{
+    static const float black [] = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+    glClearBufferfv (GL_COLOR, 0, black);
+
+    // Not sure if we need to bind it again
+    glBindVertexArray (VAOs [Triangles]);
+
+    glDrawArrays (GL_TRIANGLES, 0, NumVertices);
+}
+
+//----------------------------------------------------------------------------
+
+void cleanupUserOGL ()
+{
+}
